1871998935e8a5ad657f48f036683cd3a11f8408
wjz <weijianzhan@163.com>
2021-01-24 14:17:20 +0800

    Update Sample Code

---
+++ service/config.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/csby/gwsf/gcfg"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type Config struct {
+	sync.RWMutex
+	gcfg.Config
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Config: gcfg.Config{
+			Log: gcfg.Log{
+				Folder: "",
+				Level:  "error|warning|info",
+			},
+			Http: gcfg.Http{
+				Enabled:     true,
+				Port:        8080,
+				BehindProxy: false,
+			},
+			Https: gcfg.Https{
+				Enabled:     false,
+				Port:        8443,
+				BehindProxy: false,
+				Cert: gcfg.Crt{
+					Ca: gcfg.CrtCa{
+						File: "",
+					},
+					Server: gcfg.CrtPfx{
+						File:     "",
+						Password: "",
+					},
+				},
+				RequestClientCert: true,
+			},
+			Site: gcfg.Site{
+				Doc: gcfg.SiteDoc{
+					Enabled: true,
+				},
+			},
+		},
+	}
+}
+
+func (s *Config) LoadFromFile(filePath string) error {
+	s.Lock()
+	defer s.Unlock()
+
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(bytes, s)
+}
+
+func (s *Config) SaveToFile(filePath string) error {
+	s.Lock()
+	defer s.Unlock()
+
+	bytes, err := json.MarshalIndent(s, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	fileFolder := filepath.Dir(filePath)
+	_, err = os.Stat(fileFolder)
+	if os.IsNotExist(err) {
+		os.MkdirAll(fileFolder, 0777)
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprint(file, string(bytes[:]))
+
+	return err
+}
+
+func (s *Config) String() string {
+	bytes, err := json.Marshal(s)
+	if err != nil {
+		return ""
+	}
+
+	return string(bytes[:])
+}
+
+func (s *Config) FormatString() string {
+	bytes, err := json.MarshalIndent(s, "", "    ")
+	if err != nil {
+		return ""
+	}
+
+	return string(bytes[:])
+}
---
+++ service/handler.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/csby/gwsf/gtype"
+)
+
+func NewHandler(log gtype.Log) gtype.Handler {
+	instance := &Handler{}
+	instance.SetLog(log)
+
+	instance.apiController = &Controller{}
+	instance.apiController.SetLog(log)
+
+	return instance
+}
+
+type Handler struct {
+	gtype.Base
+
+	apiController *Controller
+}
+
+func (s *Handler) InitRouting(router gtype.Router) {
+	router.POST(apiPath.Uri("/hello"), nil, s.apiController.Hello, s.apiController.HelloDoc)
+}
+
+func (s *Handler) BeforeRouting(ctx gtype.Context) {
+	// enable across access
+	if ctx.Method() == "OPTIONS" {
+		ctx.Response().Header().Add("Access-Control-Allow-Origin", "*")
+		ctx.Response().Header().Set("Access-Control-Allow-Headers", "content-type,token")
+		ctx.SetHandled(true)
+	}
+}
+
+func (s *Handler) AfterRouting(ctx gtype.Context) {
+
+}
+
+func (s *Handler) ExtendOptApi(router gtype.Router, path *gtype.Path, preHandle gtype.HttpHandle, wsc gtype.SocketChannelCollection) {
+
+}
